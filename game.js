f27dd38865ceefc57e8d48dcad4d283948dc43c6
Nate <natewillimack@gmail.com>
2024-10-04 12:48:52 -0500

    fixed small bugs

--- game.js
+++ game.js
@@ -14,89 +14,81 @@ const config = {
       create: create,
       update: update
     }
-  };
-  
-  const game = new Phaser.Game(config);
-  
-  let player;
-  let platforms;
-  let cursors;
-  
-  function preload() {
-    // Load assets like images and spritesheets
-    this.load.image('sky', 'assets/sky.png');         // Background image
-    this.load.image('ground', 'assets/platform.png'); // Platforms
-    this.load.spritesheet('mario', 'assets/mario.png', { frameWidth: 32, frameHeight: 48 }); // Mario sprite
-  }
-  
-  function create() {
-    // Stretch the background to fit the canvas size, ensuring it scales properly
-    let background = this.add.image(0, 0, 'sky').setOrigin(0, 0); 
-    background.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height); // Stretch background
-  
-    // Create a static group of platforms
+};
+
+const game = new Phaser.Game(config);
+
+let player;
+let platforms;
+let cursors;
+
+function preload() {
+    // Load assets
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.spritesheet('mario', 'assets/mario.png', { frameWidth: 32, frameHeight: 48 });
+}
+
+function create() {
+    // Add background image
+    let background = this.add.image(0, 0, 'sky').setOrigin(0, 0);
+    background.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height);
+
+    // Add platforms
     platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody(); // Ground platform
-    platforms.create(600, 400, 'ground'); // Mid-air platforms
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
-  
-    // Create Mario player
+
+    // Add player sprite
     player = this.physics.add.sprite(100, 450, 'mario');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
-  
-    // Add player animations
+
+    // Create player animations
     this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers('mario', { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('mario', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
     });
-  
+
     this.anims.create({
-      key: 'turn',
-      frames: [{ key: 'mario', frame: 4 }],
-      frameRate: 20
+        key: 'turn',
+        frames: [{ key: 'mario', frame: 4 }],
+        frameRate: 20
     });
-  
+
     this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers('mario', { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('mario', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
     });
-  
-    // Enable player collision with platforms
+
+    // Enable collision between player and platforms
     this.physics.add.collider(player, platforms);
-  
-    // Setup keyboard controls
+
+    // Add keyboard controls
     cursors = this.input.keyboard.createCursorKeys();
-  
-    // Add a listener to start the AudioContext after user interaction
-    this.input.on('pointerdown', () => {
-      if (this.sound.context.state === 'suspended') {
-        this.sound.context.resume();
-      }
-    });
-  }
-  
-  function update() {
-    // Movement controls
+}
+
+function update() {
+    // Player movement
     if (cursors.left.isDown) {
-      player.setVelocityX(-160);
-      player.anims.play('left', true);
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
     } else if (cursors.right.isDown) {
-      player.setVelocityX(160);
-      player.anims.play('right', true);
+        player.setVelocityX(160);
+        player.anims.play('right', true);
     } else {
-      player.setVelocityX(0);
-      player.anims.play('turn');
+        player.setVelocityX(0);
+        player.anims.play('turn');
     }
-  
-    // Jumping
+
+    // Player jump
     if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
+        player.setVelocityY(-330);
     }
-  }
-  
+}
